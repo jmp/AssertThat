cff18583b95eacb86268de7f89731a52a1bf649a
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-12-20 18:11:25 +0200

    Add missing file and line

--- Sources/AssertThat/Extensions/Equatable.swift
+++ Sources/AssertThat/Extensions/Equatable.swift
@@ -12,12 +12,12 @@ public extension Assertion where Subject: Equatable {
     }
 
     @discardableResult func isIn<S>(_ sequence: S, file: StaticString = #filePath, line: UInt = #line) -> Self where S: Sequence, Subject == S.Element {
-        XCTAssertTrue(sequence.contains { $0 == subject })
+        XCTAssertTrue(sequence.contains { $0 == subject }, file: file, line: line)
         return self
     }
 
     @discardableResult func isNotIn<S>(_ sequence: S, file: StaticString = #filePath, line: UInt = #line) -> Self where S: Sequence, Subject == S.Element {
-        XCTAssertFalse(sequence.contains { $0 == subject })
+        XCTAssertFalse(sequence.contains { $0 == subject }, file: file, line: line)
         return self
     }
 }
