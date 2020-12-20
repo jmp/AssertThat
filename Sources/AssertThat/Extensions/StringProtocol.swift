5418077c868dd55ec2b279c3be765bffca975ae4
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-12-20 16:32:19 +0200

    Add assertions for StringProtocol

---
+++ Sources/AssertThat/Extensions/StringProtocol.swift
@@ -0,0 +1,43 @@
+import XCTest
+
+public extension Assertion where Subject: StringProtocol {
+    @discardableResult func startsWith(_ other: Subject, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        XCTAssertTrue(subject.starts(with: other), file: file, line: line)
+        return self
+    }
+
+    @discardableResult func doesNotStartWith(_ other: Subject, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        XCTAssertFalse(subject.starts(with: other), file: file, line: line)
+        return self
+    }
+
+    @discardableResult func endsWith(_ other: Subject, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        XCTAssertTrue(subject.hasSuffix(other), file: file, line: line)
+        return self
+    }
+
+    @discardableResult func doesNotEndWith(_ other: Subject, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        XCTAssertFalse(subject.hasSuffix(other), file: file, line: line)
+        return self
+    }
+
+    @discardableResult func contains(_ other: Subject, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        XCTAssertTrue(subject.contains(other), file: file, line: line)
+        return self
+    }
+
+    @discardableResult func doesNotContain(_ other: Subject, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        XCTAssertFalse(subject.contains(other), file: file, line: line)
+        return self
+    }
+
+    @discardableResult func matches(_ pattern: String, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        XCTAssertTrue(subject.range(of: pattern, options: .regularExpression, range: nil, locale: nil) != nil, file: file, line: line)
+        return self
+    }
+
+    @discardableResult func doesNotMatch(_ pattern: String, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        XCTAssertFalse(subject.range(of: pattern, options: .regularExpression, range: nil, locale: nil) != nil, file: file, line: line)
+        return self
+    }
+}
---
+++ Tests/AssertThatTests/StringProtocolTests.swift
@@ -0,0 +1,102 @@
+import XCTest
+import AssertThat
+
+final class StringProtocolTests: SuppressableTestCase {
+    func testStartsWithSuccess() {
+        suppress { assertThat("foo").startsWith("f") }
+        XCTAssertEqual(0, suppressedIssues)
+    }
+
+    func testStartsWithFailure() {
+        suppress { assertThat("foo").startsWith("x") }
+        XCTAssertEqual(1, suppressedIssues)
+    }
+
+    func testDoesNotStartWithSuccess() {
+        suppress { assertThat("foo").doesNotStartWith("x") }
+        XCTAssertEqual(0, suppressedIssues)
+    }
+
+    func testDoesNotStartWithFailure() {
+        suppress { assertThat("foo").doesNotStartWith("f") }
+        XCTAssertEqual(1, suppressedIssues)
+    }
+
+    func testEndsWithSuccess() {
+        suppress { assertThat("foo").endsWith("o") }
+        XCTAssertEqual(0, suppressedIssues)
+    }
+
+    func testEndsWithFailure() {
+        suppress { assertThat("foo").endsWith("x") }
+        XCTAssertEqual(1, suppressedIssues)
+    }
+
+    func testDoesNotEndWithSuccess() {
+        suppress { assertThat("foo").doesNotEndWith("x") }
+        XCTAssertEqual(0, suppressedIssues)
+    }
+
+    func testDoesNotEndWithFailure() {
+        suppress { assertThat("foo").doesNotEndWith("o") }
+        XCTAssertEqual(1, suppressedIssues)
+    }
+
+    func testContainsSuccess() {
+        suppress { assertThat("foo").contains("o") }
+        XCTAssertEqual(0, suppressedIssues)
+    }
+
+    func testContainsFailure() {
+        suppress { assertThat("foo").contains("x") }
+        XCTAssertEqual(1, suppressedIssues)
+    }
+
+    func testDoesNotContainSuccess() {
+        suppress { assertThat("foo").doesNotContain("x") }
+        XCTAssertEqual(0, suppressedIssues)
+    }
+
+    func testDoesNotContainFailure() {
+        suppress { assertThat("foo").doesNotContain("o") }
+        XCTAssertEqual(1, suppressedIssues)
+    }
+
+    func testMatchesSuccess() {
+        suppress { assertThat("foo").matches("^fo.$") }
+        XCTAssertEqual(0, suppressedIssues)
+    }
+
+    func testMatchesFailure() {
+        suppress { assertThat("foo").matches("^bar$") }
+        XCTAssertEqual(1, suppressedIssues)
+    }
+
+    func testDoesNotMatchSuccess() {
+        suppress { assertThat("foo").doesNotMatch("^bar$") }
+        XCTAssertEqual(0, suppressedIssues)
+    }
+
+    func testDoesNotMatchFailure() {
+        suppress { assertThat("foo").doesNotMatch("^fo.$") }
+        XCTAssertEqual(1, suppressedIssues)
+    }
+
+    static var allTests = [
+        ("testStartsWithSuccess", testStartsWithSuccess),
+        ("testStartsWithFailure", testStartsWithFailure),
+        ("testDoesNotStartWithSuccess", testDoesNotStartWithSuccess),
+        ("testDoesNotStartWithFailure", testDoesNotStartWithFailure),
+        ("testEndsWithSuccess", testEndsWithSuccess),
+        ("testEndsWithFailure", testEndsWithFailure),
+        ("testDoesNotEndWithSuccess", testDoesNotEndWithSuccess),
+        ("testContainsSuccess", testContainsSuccess),
+        ("testContainsFailure", testContainsFailure),
+        ("testDoesNotContainSuccess", testDoesNotContainSuccess),
+        ("testDoesNotContainFailure", testDoesNotContainFailure),
+        ("testMatchesSuccess", testMatchesSuccess),
+        ("testMatchesFailure", testMatchesFailure),
+        ("testDoesNotMatchSuccess", testDoesNotMatchSuccess),
+        ("testDoesNotMatchFailure", testDoesNotMatchFailure),
+    ]
+}
