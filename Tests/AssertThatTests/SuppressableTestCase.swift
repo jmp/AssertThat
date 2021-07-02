cbc4b5dec0c545bdd26d56a8b26b39802bef9e07
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-07-02 11:59:43 +0300

    Remove use of recordFailure API

--- Tests/AssertThatTests/SuppressableTestCase.swift
+++ Tests/AssertThatTests/SuppressableTestCase.swift
@@ -4,7 +4,6 @@ class SuppressableTestCase: XCTestCase {
     private(set) var suppressedIssues = 0
     private var isSuppressing = false
 
-    #if os(macOS) || os(iOS) || os(tvOS)
     override func record(_ issue: XCTIssue) {
         if isSuppressing {
             suppressedIssues += 1
@@ -12,20 +11,6 @@ class SuppressableTestCase: XCTestCase {
             super.record(issue)
         }
     }
-    #else
-    override func recordFailure(
-        withDescription description: String,
-        inFile filePath: String,
-        atLine lineNumber: Int,
-        expected: Bool
-    ) {
-        if isSuppressing {
-            suppressedIssues += 1
-        } else {
-            super.recordFailure(withDescription: description, inFile: filePath, atLine: lineNumber, expected: expected)
-        }
-    }
-    #endif
 
     func suppress(_ block: () -> Void) {
         suppressedIssues = 0
