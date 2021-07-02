81279d9e8354f894adfc84a14e9b9f6b46824c29
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-07-02 19:51:09 +0300

    Remove unnecessary test

--- Tests/AssertThatTests/AssertThatTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-import AssertThat
-
-final class AssertThatTests: XCTestCase {
-    func testAssertThatReturnsAssertion() {
-        let assertion = assertThat("dummy") as Any
-        XCTAssertTrue(assertion is Assertion<String>)
-    }
-}
