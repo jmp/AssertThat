c18d8fa45ed2dc6c73a09c41fba4e4d619a80f5c
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-07-02 12:02:44 +0300

    Revert to recordFailure on Linux

--- Tests/AssertThatTests/SuppressableTestCase.swift
+++ Tests/AssertThatTests/SuppressableTestCase.swift
@@ -4,6 +4,7 @@ class SuppressableTestCase: XCTestCase {
     private(set) var suppressedIssues = 0
     private var isSuppressing = false
 
+    #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
     override func record(_ issue: XCTIssue) {
         if isSuppressing {
             suppressedIssues += 1
@@ -11,6 +12,20 @@ class SuppressableTestCase: XCTestCase {
             super.record(issue)
         }
     }
+    #else
+    override func recordFailure(
+        withDescription description: String,
+        inFile filePath: String,
+        atLine lineNumber: Int,
+        expected: Bool
+    ) {
+        if isSuppressing {
+            suppressedIssues += 1
+        } else {
+            super.recordFailure(withDescription: description, inFile: filePath, atLine: lineNumber, expected: expected)
+        }
+    }
+    #endif
 
     func suppress(_ block: () -> Void) {
         suppressedIssues = 0
