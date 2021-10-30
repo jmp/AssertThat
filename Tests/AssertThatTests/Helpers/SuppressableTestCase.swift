88f96bc668ad57c78b11975ff3a6945e187ab566
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-30 18:11:33 +0300

    Simplify suppressable test case

--- Tests/AssertThatTests/Helpers/SuppressableTestCase.swift
+++ Tests/AssertThatTests/Helpers/SuppressableTestCase.swift
@@ -2,35 +2,24 @@ import XCTest
 
 class SuppressableTestCase: XCTestCase {
     private(set) var suppressedIssues = 0
-    private var isSuppressing = false
 
-    #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
     override func record(_ issue: XCTIssue) {
-        if isSuppressing {
-            suppressedIssues += 1
-        } else {
-            super.record(issue)
-        }
+        suppressedIssues += 1
     }
-    #else
+#else
     override func recordFailure(
         withDescription description: String,
         inFile filePath: String,
         atLine lineNumber: Int,
         expected: Bool
     ) {
-        if isSuppressing {
-            suppressedIssues += 1
-        } else {
-            super.recordFailure(withDescription: description, inFile: filePath, atLine: lineNumber, expected: expected)
-        }
+        suppressedIssues += 1
     }
-    #endif
+#endif
 
     func suppress(_ block: () -> Void) {
         suppressedIssues = 0
-        isSuppressing = true
-        defer { isSuppressing = false }
         block()
     }
 }
