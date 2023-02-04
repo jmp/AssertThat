96666d3876beba990396fcc98c5836d126ff0b54
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2023-02-04 22:04:18 +0200

    Add some documentation

--- Sources/AssertThat/AssertThat.swift
+++ Sources/AssertThat/AssertThat.swift
@@ -1,3 +1,13 @@
+/// Create a new assertion for the given subject.
+///
+/// The function takes a subject (e.g. an integer or an expression) as its only argument and
+/// returns an ``Assertion`` instance. The methods of the return value can be used to
+/// verify that a wanted condition is true.
+///
+/// - Parameters:
+///    - subject: The subject of the assertion
+///
+/// - Returns: A new ``Assertion`` instance for the given subject.
 public func assertThat<Subject>(_ subject: Subject) -> Assertion<Subject> {
     Assertion(subject: subject)
 }
--- Sources/AssertThat/Assertion.swift
+++ Sources/AssertThat/Assertion.swift
@@ -1,3 +1,9 @@
+/// A generic struct for making assertions.
+///
+/// Each `Assertion` has a subject whose type is defined by the generic parameter `Subject`.
+/// Assertion methods such as `isEqualTo` can be defined in extensions. The extensions
+/// may restrict `Subject` to specific protocols or types.
 public struct Assertion<Subject> {
+    /// The subject of the assertion
     public let subject: Subject
 }
